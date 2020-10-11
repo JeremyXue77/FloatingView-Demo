47350ce178d86b7e02d1a5107347d532ac1613f3
Jeremy Xue <gg831006@gmail.com>
2020-10-11 15:35:39 +0800

    Initial Commit

---
+++ FloatingView-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatingView-Demo
+//
+//  Created by Jeremy Xue on 2020/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
